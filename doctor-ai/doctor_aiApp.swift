7eb2d130a9d43fc14183b7c57c46f736a1a55349
juikong <juikong@yahoo.com>
2024-07-06 15:27:43 +0800

    Initial Commit

---
+++ doctor-ai/doctor_aiApp.swift
@@ -0,0 +1,20 @@
+//
+//  doctor_aiApp.swift
+//  doctor-ai
+//
+//  Created by Juiko Ong on 06/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct doctor_aiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ doctor-aiTests/doctor_aiTests.swift
@@ -0,0 +1,16 @@
+//
+//  doctor_aiTests.swift
+//  doctor-aiTests
+//
+//  Created by Juiko Ong on 06/07/2024.
+//
+
+import Testing
+
+struct doctor_aiTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
